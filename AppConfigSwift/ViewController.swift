489ac16ca5be640c493a3bf10781480b4505d69a
David Arnold <darnold@blackberry.com>
2018-01-19 11:57:59 +0000

    Initial Commit

---
+++ AppConfigSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppConfigSwift
+//
+//  Created by David Arnold on 19/01/2018.
+//  Copyright © 2018 David Arnold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
